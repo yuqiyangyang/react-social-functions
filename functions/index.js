46ec81023e36d8c9cb4ae4884b0971f8bb06631a
yuqi <yuqi.yang@mail.mcgill.ca>
2020-03-17 10:08:00 -0400

    validation and login route

--- functions/index.js
+++ functions/index.js
@@ -1,180 +1,20 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
 
+const FBAuth = require('./util/fbAuth')
 
-admin.initializeApp();
+const{getAllScreams, postOneScream} = require('./handlers/screams');
+const{signup,login} = require('./handlers/users');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDyCZVcOt1cLKWNrc6DHNZ-XWRuyQ9NVe8",
-    authDomain: "socialape-8c56c.firebaseapp.com",
-    databaseURL: "https://socialape-8c56c.firebaseio.com",
-    projectId: "socialape-8c56c",
-    storageBucket: "socialape-8c56c.appspot.com",
-    messagingSenderId: "819744886164",
-    appId: "1:819744886164:web:59b25d4aeb1f83d1351381",
-    measurementId: "G-CMKLQLKDXQ"
-  };
- 
-
-  const firebase = require('firebase');
-  firebase.initializeApp(firebaseConfig);
-
-const db=admin.firestore();
-
-app.get('/screams', (req, res)=>{
-    db
-    .collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data)=>{
-        let screams =[];
-        data.forEach((doc)=>{
-            screams.push({
-                screamId: doc.id,
-                body: doc.data().body,
-                userHandle: doc.data().userHandle,
-                createdAt: doc.data().createdAt,
-                commentCount:doc.data().commentCount,
-                likeCount:doc.data().likeCount
-            });
-        });
-        return res.json(screams);
-    })
-    .catch((err)=>console.error(err));
-})
-
-
-
-app.post('/scream', (req,res)=>{
-   if(req.body.body.trim()===' '){
-       return res.status(400).json({body:"body must not be empty "})
-   }
-    const newScream={
-        body:req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: new Date().toISOString()
-    };
-
-     db
-     .collection('screams')
-     .add(newScream)
-     .then((doc)=>{
-         res.json({message: `document ${doc.id} created successfully`});
-        })
-         .catch(err=>{
-             res.status(500).json({error:'somethign went wrong'});
-             console.error(err);
-     });
-});
-
-const isEmail =(email) =>{
-    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(email.match(regEx)) return true;
-    else return false;
-
-}
-const isEmpty=(string)=>{
-    if(string.trim()==='') return true;
-    else return false;
-}
-
-
-
-
+//scream rountes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream );
 //sigup route
-app.post('/signup', (req,res)=>{
-    const newUser={
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
-    };
-
-    let errors ={};
-
-    if(isEmpty(newUser.email)){
-        errors.email ='Must not be empty'
-    } else if(!isEmail(newUser.email)){
-        errors.email = 'Must be a valid email address'
-    }
-
-    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
-    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword ='Password must match';
-    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
-    if(Object.keys(errors).length >0) return res.status(400).json(errors);
-   
-    
-    //TODO Validate data
-    let token, userId;
-    db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then((doc) =>{
-        if(doc.exists){
-            return res.status(400).json({handle:'this handle is already taken'});
-        } else {
-            return firebase
-            .auth()
-            .createUserWithEmailAndPassword(newUser.email, newUser.password);}
-    })
-    .then((data)=>{
-        userId=data.user.uid;
-        return data.user.getIdToken();
-    })
-    .then((idToken)=>{
-        token=idToken;
-        const userCredentials ={
-            handle:newUser.handle,
-            email: newUser.email,
-            createdAt: new Date().toISOString(),
-            userId
-        };
-        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(()=>{
-        return res.status(201).json({token})
-    })
-    .catch(err=>{
-        console.error(err);
-        if(err.code === 'auth/email-already-in-use'){
-            return res.status(400).json({email:'email is already in use'});
-        }else{
-            res.status(500).json({error:err.code});
-        } 
-    }); 
-});
+app.post('/signup', signup);
+//login route
+app.post('/login', login);
 
 
-
-app.post('/login', (req,res)=>{
-    const user ={
-        email: req.body.email,
-        password: req.body.password
-    };
-
-    let errors ={};
-
-    if(isEmpty(user.email)) errors.email='Must not be empty';
-    if(isEmpty(user.password)) errors.password = 'Must not be empty';
-
-    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-    firebase
-    .auth()
-    .signInWithEmailAndPassword(user.email, user.password)
-    .then((data)=>{
-        return data.user.getIdToken();
-    })
-    .then(token=>{
-        return res.json({token});
-    })
-    .catch((err)=>{
-        console.error(err);
-        if(err.code === 'auth/wrong-password'){
-            return res.status(403).json({general:'Wrong credentials , please try again'});
-        }else{
-        return res.status(500).json({error:err.code})}
-    });
-});
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyDyCZVcOt1cLKWNrc6DHNZ-XWRuyQ9NVe8",
+    authDomain: "socialape-8c56c.firebaseapp.com",
+    databaseURL: "https://socialape-8c56c.firebaseio.com",
+    projectId: "socialape-8c56c",
+    storageBucket: "socialape-8c56c.appspot.com",
+    messagingSenderId: "819744886164",
+    appId: "1:819744886164:web:59b25d4aeb1f83d1351381",
+    measurementId: "G-CMKLQLKDXQ"
+  };
+ 
